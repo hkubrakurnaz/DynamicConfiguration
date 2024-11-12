60092d5c3d4ac5642be6aee3426542fc14d010da
hkubrakurnaz <hkubrakurnaz@gmail.com>
2024-11-12 11:54:58 +0300

    add demo

---
+++ package/Demo/Demo/Controllers/ConfigurationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationsController : ControllerBase
+    {
+        private readonly IConfigurationReader _configurationReader;
+
+        public WeatherForecastController( IConfigurationReader configurationReader)
+        {
+            _configurationReader = configurationReader;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _configurationReader.GetValue<string>("test2");
+        }
+    }
+}
